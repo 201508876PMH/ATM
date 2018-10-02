40e843dd2925f61bc99c3092888bbd9cad9309dd
Peter Marcus Hoveling <au536878@uni.au.dk>
2018-10-02 13:13:24 +0200

    Added classes

--- ATM/classes/Log.cs
+++ ATM/classes/Log.cs
@@ -4,12 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ATM.Test.Unit
+namespace ATM.classes
 {
-    public class ATMTest
+    class Log
     {
-       
-        //test
-
     }
 }
