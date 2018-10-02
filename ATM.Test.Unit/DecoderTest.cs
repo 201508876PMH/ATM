f7233c30e8f23c13757efbf3ac6aee1632b0e34b
Peter Marcus Hoveling <au536878@uni.au.dk>
2018-10-02 15:35:59 +0200

    Added testcases to Decoder

--- ATM.Test.Unit/DecoderTest.cs
+++ ATM.Test.Unit/DecoderTest.cs
@@ -46,8 +46,31 @@ namespace ATM.Test.Unit
 
             holderList = _uut.CloneList(fakeListFull);
 
-            Assert.That(fakeListEmpty, Is.EqualTo(holderList));
+            Assert.That(fakeListEmpty, Is.Not.AnyOf(holderList));
         }
 
+        [Test]
+        // Test that when given a empty list, we receive 
+        public void runSelfTest_cloneListSuccedes()
+        {
+            List<AircraftData> fakeListEmpty = new List<AircraftData>();
+            List<AircraftData> holderList = new List<AircraftData>();
+
+            List<AircraftData> fakeListFull = new List<AircraftData>();
+            fakeListFull.Add(new AircraftData("FlIGHT01", 8001, 40001, 10001, new TimeStamp(2018, 10, 2, 14, 0, 0, 0)));
+            fakeListFull.Add(new AircraftData("FLIGHT02", 8002, 40002, 10002, new TimeStamp(2019, 11, 3, 15, 1, 1, 2)));
+            fakeListFull.Add(new AircraftData("FLIGHT03", 8003, 40003, 10003, new TimeStamp(2010, 12, 4, 16, 2, 2, 3)));
+
+            holderList = _uut.CloneList(fakeListFull);
+
+            Assert.That(fakeListFull, Is.EqualTo(holderList));
+        }
+
+
+        [Test]
+        public void runSelfTest_DecodeString()
+        {
+
+        }
     }
 }
