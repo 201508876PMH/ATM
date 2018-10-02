c36850a49132e8db0a666fbd7753be9c607711d0
Peter Marcus Hoveling <au536878@uni.au.dk>
2018-10-02 15:21:36 +0200

    Updated decoder Test

--- ATM.Test.Unit/DecoderTest.cs
+++ ATM.Test.Unit/DecoderTest.cs
@@ -3,14 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ATM.interfaces;
+using NSubstitute;
 using NUnit.Framework;
+using Decoder = ATM.classes.Decoder;
+using ATM.classes;
 
 namespace ATM.Test.Unit
 {
     [TestFixture]
-    class DecoderTest
+    public class DecoderTest
     {
         private Decoder _uut;
+        private IUtility _utility;
 
         [SetUp]
         public void setUp()
@@ -20,16 +25,29 @@ namespace ATM.Test.Unit
              */
 
             //if we test multiple classes in conjunction, we add mocks here:
-            //_idoor = Substitute.For<IDoor>();
-
-            //Constructor injection
-            //_uut = new DoorControl(_userValidation, _alarm, _idoor, _entryNot);
-
-
+            _utility = Substitute.For<IUtility>();
             
+            
+            //Constructor injection
+            _uut = new Decoder(_utility);
         }
 
-       
+        [Test]
+        // Test that when given a empty list, we receive 
+        public void runSelfTest_cloneListFails()
+        {
+            List<AircraftData> fakeListEmpty = new List<AircraftData>();
+            List<AircraftData> holderList = new List<AircraftData>();
+
+            List<AircraftData> fakeListFull = new List<AircraftData>();
+            fakeListFull.Add(new AircraftData("FlIGHT01", 8001, 40001, 10001, new TimeStamp(2018, 10, 2, 14, 0, 0, 0)));
+            fakeListFull.Add(new AircraftData("FLIGHT02", 8002, 40002, 10002, new TimeStamp(2019, 11, 3, 15, 1, 1, 2)));
+            fakeListFull.Add(new AircraftData("FLIGHT03", 8003, 40003, 10003, new TimeStamp(2010, 12, 4, 16, 2, 2, 3)));
+
+            holderList = _uut.CloneList(fakeListFull);
+
+            Assert.That(fakeListEmpty, Is.EqualTo(holderList));
+        }
 
     }
 }
