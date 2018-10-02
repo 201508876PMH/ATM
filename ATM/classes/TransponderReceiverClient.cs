e3149c2684e5ccceca581f2832599c83d1b0cc80
anton <antonsihm@gmail.com>
2018-10-02 13:30:57 +0200

    TransponerRechiever Client Constructor Injection

--- ATM/classes/TransponderReceiverClient.cs
+++ ATM/classes/TransponderReceiverClient.cs
@@ -1,33 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using TransponderReceiver;
+﻿using TransponderReceiver;
 
 namespace ATM.classes
 {
     class TransponderReceiverClient
     {
-        private ITransponderReceiver receiver;
+        private ITransponderReceiver _receiver;
+        private IAnalyser _analyser;
+        private IDecoder _decoder;
 
-        // Using constructor injection for dependency/ies
-        public TransponderReceiverClient(ITransponderReceiver receiver)
+        // Using constructor injection
+        public TransponderReceiverClient(ITransponderReceiver receiver, IAnalyser analyser, IDecoder decoder)
         {
-            // This will store the real or the fake transponder data receiver
-            this.receiver = receiver;
+            // set Interface fields // Fakes or real
+            _receiver = receiver;
+            _analyser = analyser;
+            _receiver = receiver;
+
 
             // Attach to the event of the real or the fake TDR
-            this.receiver.TransponderDataReady += ReceiverOnTransponderDataReady;
+            _receiver.TransponderDataReady += ReceiverOnTransponderDataReady;
         }
 
         private void ReceiverOnTransponderDataReady(object sender, RawTransponderDataEventArgs e)
         {
-            // Just display data
-            foreach (var data in e.TransponderData)
-            {
-                System.Console.WriteLine($"Transponderdata {data}");
-            }
+            // Decode data
+            
+
+            
+
+
+
         }
         
     }
