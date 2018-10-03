e99a0c9c3e8ff588a1227d16e4c182bd74fccc36
Peter Marcus Hoveling <au536878@uni.au.dk>
2018-10-03 09:50:22 +0200

    added more test methods for Decoder

--- ATM.Test.Unit/DecoderTest.cs
+++ ATM.Test.Unit/DecoderTest.cs
@@ -16,6 +16,7 @@ namespace ATM.Test.Unit
     {
         private Decoder _uut;
         private IUtility _utility;
+        
 
         [SetUp]
         public void setUp()
@@ -53,7 +54,6 @@ namespace ATM.Test.Unit
         // Test that when given a empty list, we receive 
         public void runSelfTest_cloneListSuccedes()
         {
-            List<AircraftData> fakeListEmpty = new List<AircraftData>();
             List<AircraftData> holderList = new List<AircraftData>();
 
             List<AircraftData> fakeListFull = new List<AircraftData>();
@@ -70,7 +70,104 @@ namespace ATM.Test.Unit
         [Test]
         public void runSelfTest_DecodeString()
         {
+           string stringToDecode = "ATR423;39045;12932;14000;20151006213456789";
+           AircraftData holder;
+
+           holder = _uut.DecodeString(stringToDecode);
+
+            Assert.That(holder.Tag == "ATR423");
+            Assert.That(holder.X_coordinate == int.Parse("39045"));
+            Assert.That(holder.Y_coordinate == int.Parse("12932"));
+            Assert.That(holder.Altitude == int.Parse("14000"));
+           
+         
+            Assert.That(holder.TimeStamp.ms == 789);
+            Assert.That(holder.TimeStamp.sec == 56);
+            Assert.That(holder.TimeStamp.min == 34);
+            Assert.That(holder.TimeStamp.hour == 21);
+            Assert.That(holder.TimeStamp.day == 06);
+            Assert.That(holder.TimeStamp.month == 10);
+            Assert.That(holder.TimeStamp.year == 2015);
+            
 
         }
+
+        [Test]
+
+        public void runSelfTest_InsertSpeedWhereListsAreEqual()
+        {
+            // After this test, all the new objects should have the speed of the old objects
+
+            List<AircraftData> oldFakeList = new List<AircraftData>();
+            oldFakeList.Add(new AircraftData("FlIGHT01", 0001, 00001, 00001, new TimeStamp(0001, 01, 1, 1, 1, 1, 1)));
+            oldFakeList.Add(new AircraftData("FlIGHT02", 0002, 00002, 00002, new TimeStamp(0002, 02, 2, 2, 2, 2, 2)));
+            oldFakeList.Add(new AircraftData("FlIGHT03", 0003, 00003, 00003, new TimeStamp(0003, 03, 3, 3, 3, 3, 3)));
+
+            List<AircraftData> newFakeList = new List<AircraftData>();
+            newFakeList.Add(new AircraftData("FlIGHT01", 0000, 00000, 00001, new TimeStamp(0001, 01, 1, 1, 1, 1, 1)));
+            newFakeList.Add(new AircraftData("FlIGHT02", 0000, 00000, 00002, new TimeStamp(0002, 02, 2, 2, 2, 2, 2)));
+            newFakeList.Add(new AircraftData("FlIGHT03", 0000, 00000, 00003, new TimeStamp(0003, 03, 3, 3, 3, 3, 3)));
+
+            _uut.InsertSpeed(oldFakeList, newFakeList);
+
+            Assert.That(newFakeList[0].Speed == oldFakeList[0].Speed);
+            Assert.That(newFakeList[1].Speed == oldFakeList[1].Speed);
+            Assert.That(newFakeList[2].Speed == oldFakeList[2].Speed);
+        }
+
+        [Test]
+        public void runSelfTest_InsertSpeedWhereOldListIsGreater()
+        {
+            // After this test, all the new objects should have the speed of the old objects
+
+            List<AircraftData> oldFakeList = new List<AircraftData>();
+            oldFakeList.Add(new AircraftData("FlIGHT01", 0001, 00001, 00001, new TimeStamp(0001, 01, 1, 1, 1, 1, 1)));
+            oldFakeList.Add(new AircraftData("FlIGHT02", 0002, 00002, 00002, new TimeStamp(0002, 02, 2, 2, 2, 2, 2)));
+            oldFakeList.Add(new AircraftData("FlIGHT03", 0003, 00003, 00003, new TimeStamp(0003, 03, 3, 3, 3, 3, 3)));
+
+            List<AircraftData> newFakeList = new List<AircraftData>();
+            newFakeList.Add(new AircraftData("FlIGHT01", 0000, 00000, 00001, new TimeStamp(0001, 01, 1, 1, 1, 1, 1)));
+            newFakeList.Add(new AircraftData("FlIGHT02", 0000, 00000, 00002, new TimeStamp(0002, 02, 2, 2, 2, 2, 2)));
+            
+
+            _uut.InsertSpeed(oldFakeList, newFakeList);
+
+            Assert.That(newFakeList[0].Speed == oldFakeList[0].Speed);
+            Assert.That(newFakeList[1].Speed == oldFakeList[1].Speed);
+            
+        }
+
+        [Test]
+        public void runSelfTest_InsertSpeedWhereOldListIsLess()
+        {
+            // After this test, all the new objects should have the speed of the old objects
+
+            List<AircraftData> oldFakeList = new List<AircraftData>();
+            oldFakeList.Add(new AircraftData("FlIGHT01", 0001, 00001, 00001, new TimeStamp(0001, 01, 1, 1, 1, 1, 1)));
+            oldFakeList.Add(new AircraftData("FlIGHT02", 0002, 00002, 00002, new TimeStamp(0002, 02, 2, 2, 2, 2, 2)));
+
+
+            List<AircraftData> newFakeList = new List<AircraftData>();
+            newFakeList.Add(new AircraftData("FlIGHT01", 0000, 00000, 00001, new TimeStamp(0001, 01, 1, 1, 1, 1, 1)));
+            newFakeList.Add(new AircraftData("FlIGHT02", 0000, 00000, 00002, new TimeStamp(0002, 02, 2, 2, 2, 2, 2)));
+            newFakeList.Add(new AircraftData("FlIGHT03", 0000, 00000, 00003, new TimeStamp(0003, 03, 3, 3, 3, 3, 3)));
+
+            _uut.InsertSpeed(oldFakeList, newFakeList);
+
+            Assert.That(newFakeList[0].Speed == oldFakeList[0].Speed);
+            Assert.That(newFakeList[1].Speed == oldFakeList[1].Speed);
+
+        }
+
+        [Test]
+        public void runSelfTest_testGetterOfAircrafts()
+        {
+            AircraftData AC1 = new AircraftData("FLIGHT01", 0001, 00001, 00001, new TimeStamp(0001, 01, 1, 1, 1, 1, 1));
+            List<AircraftData> holderList = new List<AircraftData>();
+            holderList.Add(AC1);
+           
+            //Assert.That(holderList.;
+        }
+
     }
 }
