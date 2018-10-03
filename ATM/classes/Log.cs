0fbd53cf22bc9a5cd1de9a5626da5fc98cee00e9
Oliver Monberg <au554208@uni.au.dk>
2018-10-03 09:27:31 +0200

    100p code coverage in class Analyser

--- ATM/classes/Log.cs
+++ ATM/classes/Log.cs
@@ -1,12 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ATM.interfaces;
 
 namespace ATM.classes
 {
-    class Log
+    public class Log : ILog
     {
+        public Log()
+        {
+            File.Create("log.txt");
+        }
+
+        public void LogSeperationEvent(AircraftData a1, AircraftData a2)
+        {
+            File.AppendAllText(@"log.txt", $"WARNING! Possible collision between flight {a1.Tag} and {a2.Tag}."
+                                           + Environment.NewLine + a1.ToString() + Environment.NewLine + a2.ToString() + Environment.NewLine + Environment.NewLine);
+        }
     }
 }
