1009bdf36769606fbefb3f9c815fdb2142af75de
anton <antonsihm@gmail.com>
2018-11-08 14:30:34 +0100

    DecoderAndAnalyserIntegrationTest: - Done and tested  properly

--- ATM.Test.Unit/IntegrationTests/DecoderAndAnalyser.cs
+++ ATM.Test.Unit/IntegrationTests/DecoderAndAnalyser.cs
@@ -70,20 +70,34 @@ namespace ATM.Test.Unit.IntegrationTests
         public void TestSeparationEventRaised()
         {
             // SetUp Event
-            string testerString = "ATR423;30000;31111;14000;20151006213456789";
+            string testerString  = "ATR423;30000;31111;14000;20151006213456789";
             string testerString1 = "ATR424;31111;30000;14100;20151006213456789";
-            
+            string testerString2 = "ATR425;50000;30000;18000;20151006213456789";
+
             List<string> fakeEventList = new List<string>();
             fakeEventList.Add(testerString);
             fakeEventList.Add(testerString1);
-
+            fakeEventList.Add(testerString2);
+            
+            //Raise Event
             RawTransponderDataEventArgs arg = new RawTransponderDataEventArgs(fakeEventList);
             _fakeReceiver.TransponderDataReady += Raise.EventWith(_fakeReceiver, arg);
 
-
-           // _realUtility.CalcDistance(null, null).ReturnsForAnyArgs(3000);
-           
+            // Expect a Separation Event
             Assert.That(_nSeparationEventsRaised == 1);
+
+            //Raise Another Event 
+            string testerString3 = "ATR426;82222;83333;14100;20151006213456789";
+            string testerString4 = "ATR427;83333;82222;14200;20151006213456789";
+            fakeEventList.Add(testerString3);
+            fakeEventList.Add(testerString4);
+
+            RawTransponderDataEventArgs arg1 = new RawTransponderDataEventArgs(fakeEventList);
+            _fakeReceiver.TransponderDataReady += Raise.EventWith(_fakeReceiver, arg1);
+
+            // Expect a Separation Event 1 + 2
+
+            Assert.That(_nSeparationEventsRaised == 3);
             
         }
 
@@ -91,8 +105,8 @@ namespace ATM.Test.Unit.IntegrationTests
         public void TestEnterEventRaised()
         {
 
+            // SetUp
             string testerString = "ATR423;30000;31111;14000;20151006213456789";
-
             List<string> fakeEventList = new List<string>();
             fakeEventList.Add(testerString);
 
@@ -100,13 +114,76 @@ namespace ATM.Test.Unit.IntegrationTests
             _fakeReceiver.TransponderDataReady += Raise.EventWith(_fakeReceiver, arg);
 
 
-            // _realUtility.CalcDistance(null, null).ReturnsForAnyArgs(3000);
+            Assert.That(_nTrackEnteredAirspaceEventsRaised == 1);
 
-            Assert.That(_nSeparationEventsRaised == 1);
+            string testerString2 = "ATR424;30000;31111;14000;20151006213456789";
+            string testerString3 = "ATR425;30000;31111;14000;20151006213456789";
+            fakeEventList.Add(testerString2);
+            fakeEventList.Add(testerString3);
+
+            RawTransponderDataEventArgs arg2 = new RawTransponderDataEventArgs(fakeEventList);
+            _fakeReceiver.TransponderDataReady += Raise.EventWith(_fakeReceiver, arg2);
 
 
+            Assert.That(_nTrackEnteredAirspaceEventsRaised == 3);
+        }
+
+        [Test]
+        public void TestExitEventRaisedAndAnalysedDataReaddy()
+        {
+            // SetUp
+            string testerString = "ATR423;30000;31111;14000;20151006213456789";
+            string testerString2 = "ATR424;30000;31111;14000;20151006213456789";
+            string testerString3 = "ATR425;30000;31111;14000;20151006213456789";
+            string testerString4 = "ATR426;30000;31111;14000;20151006213456789";
+            string testerString5 = "ATR427;30000;31111;14000;20151006213456789";
+
+            List<string> fakeEventList = new List<string>();
+            fakeEventList.Add(testerString);
+            fakeEventList.Add(testerString2);
+            fakeEventList.Add(testerString3);
+            fakeEventList.Add(testerString4);
+            fakeEventList.Add(testerString5);
+            
+
+            RawTransponderDataEventArgs arg = new RawTransponderDataEventArgs(fakeEventList);
+            _fakeReceiver.TransponderDataReady += Raise.EventWith(_fakeReceiver, arg);
+
+            // Check that 5 track entered
+            Assert.That(_nTrackEnteredAirspaceEventsRaised == 5);
+
+            // Insure no tracks has left yet.
+            Assert.That(_nTrackLeftAirspaceEventsRaised == 0);
+            
+            // Check that Data only has been analysed one time
+            Assert.That(_nAnalysedDataReadyEventsRaised == 1);
 
 
+            // SetUp To Remove one track
+            string testerString6 = "ATR423;30000;31111;14000;20151006213456789";
+            string testerString7 = "ATR424;30000;31111;14000;20151006213456789";
+            string testerString8 = "ATR425;30000;31111;14000;20151006213456789";
+            string testerString9 = "ATR426;30000;31111;14000;20151006213456789";
+           
+            List<string> fakeEventList2 = new List<string>();
+            fakeEventList2.Add(testerString6);
+            fakeEventList2.Add(testerString7);
+            fakeEventList2.Add(testerString8);
+            fakeEventList2.Add(testerString9);
+
+            RawTransponderDataEventArgs arg2 = new RawTransponderDataEventArgs(fakeEventList2);
+            _fakeReceiver.TransponderDataReady += Raise.EventWith(_fakeReceiver, arg2);
+
+
+            // Check that 5 track entered
+            Assert.That(_nTrackEnteredAirspaceEventsRaised == 5);
+
+            // Insure no tracks has left yet.
+            Assert.That(_nTrackLeftAirspaceEventsRaised == 1);
+
+            // Check that Data only has been analysed one time
+            Assert.That(_nAnalysedDataReadyEventsRaised == 2);
+            
         }
 
 
