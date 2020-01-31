1527c77a9e06f2af1c1ed1537a6467e14ba03e2c
Peter Marcus Hoveling <42763743+201508876PMH@users.noreply.github.com>
2020-01-31 01:14:02 +0100

    Update Decoder.cs

--- ATM/classes/Decoder.cs
+++ ATM/classes/Decoder.cs
@@ -9,7 +9,7 @@ using TransponderReceiver;
 
 namespace ATM.classes
 {
-    public class Decoder : IDecoder
+    public class De0coder : IDecoder
     {
         private IUtility _utility;
         private ITransponderReceiver _receiver;
